20c76c09a19d198a89c5264145fc57a6e5757948
yejin <yejin@ibleaders.co.kr>
2021-02-21 18:16:02 +0900

    #2.1 컴포넌트의 데이터 전달 방법
    - react에서 component에 데이터 전달 방법 및 component 재사용 방법
    - es6 문법을 사용해서 컴포넌트 정의하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Potato from './Potato';
+
+// es5 문법 -> es6 문법
+// function Food(props) {
+//   console.log(props.fav);
+function Food({ fav }) {
+  return <h1> I like {fav}</h1>
+}
 
 function App() {
   return (
     <div>
       <h3>Hello!!!</h3>
-      <Potato/>
+      <Food 
+        fav= "Kimchi" 
+        // something= {true} 
+        // papapapa= {["hello", 1, 2, 3, 4, true]}
+      />
+      <Food fav= "ramen" />
     </div>
   );
 }
