f3954264122b03d2796535c6d40f1ee03d266f20
yejin <yejin@ibleaders.co.kr>
2021-02-21 18:52:11 +0900

    #2.2 동적 데이터 추가
    - 배열을 이용한 동적 데이터 추가
    - javascript map function에 대한 이해: array의 각 item에 function을 적용하고 array를 다시 리턴해줌
    - map을 이용해서 component를 동적으로 여러개 출력해 줄 수 있음.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,42 @@ import React from 'react';
 // es5 문법 -> es6 문법
 // function Food(props) {
 //   console.log(props.fav);
-function Food({ fav }) {
-  return <h1> I like {fav}</h1>
+function Penthouse({ name, img }) {
+  return (
+    <div>
+      <h2> 이름: {name} </h2>
+      <img src={img}></img>
+    </div>
+  );
 }
+
+const Characters = [
+  {
+    name: "주단태",
+    image: "./img/Dantae_Ju.jpg"
+  },
+  {
+    name: "천서진",
+    image: "./img/Chun_Seo_jin.jpg"
+  },
+  {
+    name: "오윤희",
+    image: "./img/Oh_Yoon_Hee.jpg"
+  },
+  {
+    name: "하윤철",
+    image: "./img/Ha_Yoon_chul.jpg"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h3>Hello!!!</h3>
-      <Food 
-        fav= "Kimchi" 
-        // something= {true} 
-        // papapapa= {["hello", 1, 2, 3, 4, true]}
-      />
-      <Food fav= "ramen" />
+      <h3>펜트하우스 소개</h3>
+      {Characters.map(char => (
+            <Penthouse name={char.name} img={char.image}/>
+          )
+        )}
     </div>
   );
 }
