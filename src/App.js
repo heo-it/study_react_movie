5bae0e3ac6036f1e9fd849b4a1a7fb881e1bfa8f
yejin <yejin@ibleaders.co.kr>
2021-02-21 19:08:29 +0900

    map의 key property
    - map에서 사용하는 item들은 유일한 key값을 가져야함. 여기서 key는 react내부에서 사용하기 때문에 따로 배열 출력시 보여지진 않음.

--- src/App.js
+++ src/App.js
@@ -7,41 +7,42 @@ function Penthouse({ name, img }) {
   return (
     <div>
       <h2> 이름: {name} </h2>
-      <img src={img}></img>
+      <img src={img} alt={name}></img>
     </div>
   );
 }
 
 const Characters = [
   {
+    id: 1,
     name: "주단태",
     image: "./img/Dantae_Ju.jpg"
   },
   {
+    id: 2,
     name: "천서진",
     image: "./img/Chun_Seo_jin.jpg"
   },
   {
+    id: 3,
     name: "오윤희",
     image: "./img/Oh_Yoon_Hee.jpg"
   },
   {
+    id: 4,
     name: "하윤철",
     image: "./img/Ha_Yoon_chul.jpg"
   }
 ];
 
-function renderPenthouse(char) {
-  return <Penthouse name={char.name} img={char.image} />
-}
-
 function App() {
   return (
     <div>
       <h3>펜트하우스 소개</h3>
-      {console.log("React conponent의 내부 확인")}
-      {console.log(Characters.map(renderPenthouse))}
-      {Characters.map(renderPenthouse)}
+      {Characters.map(char => (
+            <Penthouse key={char.id} name={char.name} img={char.image}/>
+          )
+        )}
     </div>
   );
 }
